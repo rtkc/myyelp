f6813fed2b58e734e879ed478cfd3c171251318c
Ruby Tang <rubykct@gmail.com>
2016-02-02 16:01:41 +0800

    Add more seeds

--- myyelp/db/seeds.rb
+++ myyelp/db/seeds.rb
@@ -8,9 +8,14 @@
 
 Category.create!(name: "Food")
 Category.create!(name: "Drinks")
+Category.create!(name: "Shopping")
+Category.create!(name: "Active Life")
+Category.create!(name: "Arts & Entertainment")
 
 Business.create!(name: "Motorino", description: "Rustic pizza", category_id: 1, small_cover_url: "/tmp/MO.jpg")
 Business.create!(name: "Jumbo", description: "Floating restaurant", category_id: 1, small_cover_url: "/tmp/Jumbo.jpg")
 Business.create!(name: "Blue Hill", description: "Magical dining hall", category_id: 1, small_cover_url: "/tmp/BlueHill.jpg")
 
 Business.create!(name: "MO Bar", description: "Mandarin Oriental bar", category_id: 2, small_cover_url: "/tmp/MO-Bar.jpg")
+
+Review.create!(business_id: 1, user_id: 1, rating: 4, review: "Food was delicious!")
